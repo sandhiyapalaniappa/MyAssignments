b8de4f628ebbd6aa35801c45bd98d719d32d97b9
sandhiyapalaniappa <60645308+sandhiyapalaniappa@users.noreply.github.com>
2023-01-22 13:13:14 +0530

    Add files via upload

---
+++ EditLead.java
@@ -0,0 +1,39 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EditLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup(); 
+		 ChromeDriver driver=new ChromeDriver();
+		 driver.get("http://leaftaps.com/opentaps/control/main");
+		 driver.manage().window().maximize();
+		    driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+			driver.findElement(By.id("password")).sendKeys("crmsfa");
+			driver.findElement(By.className("decorativeSubmit")).click();
+			driver.findElement(By.linkText("CRM/SFA")).click();
+			driver.findElement(By.linkText("Leads")).click();
+			driver.findElement(By.linkText("Create Lead")).click();
+			driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+			driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Sandhiya");
+			driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Palaniappa");
+			//driver.findElement(By.className("smallSubmit")).click();
+			driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Accountant");
+			driver.findElement(By.id("createLeadForm_description")).sendKeys("Accounts Field");
+			driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("sandhiyapalaniappa@gmail.com");
+			driver.findElement(By.className("smallSubmit")).click();
+		    driver.findElement(By.linkText("Edit")).click();
+		    driver.findElement(By.id("updateLeadForm_description")).clear();
+		    driver.findElement(By.id("updateLeadForm_importantNote")).sendKeys("Adjusting Accounts");	
+		    driver.findElement(By.className("smallSubmit")).click();
+		    System.out.println("The Title is :"+driver.getTitle());
+		    
+	}
+
+}
